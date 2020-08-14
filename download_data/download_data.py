947ef475f45312f4ca98f745a5d1cb7c292be13e
Louis de Charson <louisdecharson@posteo.net>
2020-08-14 20:51:43 +0200

    Refactor downloading SOS medecin to process raw data

--- download_data/download_data.py
+++ download_data/download_data.py
@@ -6,9 +6,26 @@ Description: Download covid related data from data.gouv.fr
 """
 import requests
 from typing import Dict, List, Any
+import pandas as pd
+import numpy as np
+
+# ============================================================================ #
 
 SLUG_DATASET = "5e74ecf52eb7514f2d3b8845"
 URL_DATASET = "https://www.data.gouv.fr/api/1/datasets/{}/".format(SLUG_DATASET)
+
+COLUMNS_MAPPING = {
+    "date_de_passage":"Day",
+    "reg": "Region",
+    "sursaud_cl_age_corona": "Age group",
+    "nbre_pass_corona": "Urgent care attendances for suspicion of COVID-19",
+    "nbre_pass_tot": "Total urgent care attendances",
+    "nbre_hospit_corona": "Hospitalizations",
+    "nbre_acte_corona": "Medical acts (SOS Médecin) for suspicion of COVID-19",
+    "nbre_acte_tot": "Medical acts total (SOS Médecin)"
+}
+OUT_RAW_FILE = "sursaud_corona_raw.csv"
+OUT_PROCESSED_FILE = "sursaud_corona.csv"
 
 # ============================================================================ #
 
@@ -24,7 +41,7 @@ class FormatError(Exception):
 # ============================================================================ #
 
 
-def download_data():
+def download_data() -> None:
     """Download SOS Medecins data from data-gouv.
     
     1. Retrieve the list of datasets
@@ -33,8 +50,50 @@ def download_data():
     
     """
     datasets = retrieve_datasets()
-    url_data = [d["url"] for d in datasets if "france.csv" in d["title"]][0]
+    url_data = [d["url"] for d in datasets if "sursaud-corona-quot-reg" in d["title"]][
+        0
+    ]
     download_csv(url_data)
+
+    # Process data
+    df = process_data(OUT_RAW_FILE)
+    print(df.head())
+    df.to_csv(OUT_PROCESSED_FILE, index=None)
+    print("{} saved to disk.".format(OUT_PROCESSED_FILE))
+
+def process_data(filepath: str) -> None:
+    # Read the csv file
+    df = pd.read_csv(filepath, sep=";")
+
+    # Select the columns
+    df = df[list(COLUMNS_MAPPING.keys())]
+    df.rename(columns=COLUMNS_MAPPING, inplace=True)
+    
+    # Compute total region
+    df_total = (df.groupby(["Day", "Age group"])
+                .sum().reset_index())
+    df_total['Region'] = 'National'
+    df_merged = pd.concat([df, df_total])
+    idx = df_merged.set_index(['Region','Age group','Day']).index
+    
+    # Compute diff
+    df_with_diff = (df_merged
+                    .sort_values(by=['Region', 'Age group','Day'])
+                    .groupby(['Region','Age group'])
+                    .diff())
+    df_with_diff.index = idx
+    df_with_diff.rename(columns={f"{c}": f"New {c}"
+                                 for c in df_with_diff.columns},
+                        inplace=True)
+    df_with_diff.reset_index(inplace=True)
+    df_total = df_merged.merge(df_with_diff,
+                               on=["Region", "Age group", "Day"])
+    df_long = pd.melt(df_total,
+                      id_vars=["Region", "Age group", "Day"],
+                      value_vars=[c for c in df_total.columns
+                                  if c not in ["Region", "Age group", "Day"]],
+                      var_name = 'Category')
+    return df_long
 
 
 def retrieve_datasets() -> List[Dict[str, str]]:
@@ -61,7 +120,7 @@ def retrieve_datasets() -> List[Dict[str, str]]:
     return r_json["resources"]
 
 
-def download_csv(url: str, name: str = "sursaud_corona.csv") -> None:
+def download_csv(url: str, name: str = OUT_RAW_FILE) -> None:
     """Download a csv file at url and save it.
     
     Args:
@@ -73,9 +132,10 @@ def download_csv(url: str, name: str = "sursaud_corona.csv") -> None:
     """
     r = requests.get(url)
     if r.status_code == 200:
+        print("Download {}".format(url.split("/")[-1]))
         with open(name, "wb") as f:
             f.write(r.content)
-            print("Download {}, saved as {}".format(url.split("/")[-1], name))
+            print("{} saved to disk.".format(name))
     else:
         raise RequestError("Error {} during download.".format(r.status_code))
 
